a25aa3c20448c588b981028e22609a065822bdbd
Alexandre Pinto @ 42 <apinto@student.42lisboa.com>
2021-07-28 13:23:08 +0100

    accomplished nothing! 😃 but with the great company of tiago! 😍

--- srcs/execute_command.c
+++ srcs/execute_command.c
@@ -6,49 +6,52 @@
 /*   By: apinto <apinto@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/27 00:26:20 by apinto            #+#    #+#             */
-/*   Updated: 2021/07/28 10:12:46 by apinto           ###   ########.fr       */
+/*   Updated: 2021/07/28 13:22:48 by apinto           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
 
 /* dup2(fd_1, fd_2)
- * fd_2 will be overwritten with fd_1 */
+ * fd_1 will be overwritten with fd_2
+ *  fd[0]; //-> for using read end
+    fd[1]; //-> for using write end*/
 int execute_command(s_info *info, char **command)
 {
 	int pid1;
 	int pid2;
 	int err;
 	int fd[2];
-	char **command_2;
 
 	if (pipe(fd) == -1)
 		return (-1);
-	dup2(STDIN, info->infile_fd);
 	pid1 = fork();
 	if (pid1 == 0)
 	{
 		if (DEBUG)
-			printf("executing %s\n", command[0]);
+		{
+			printf("executing %s\n", info->concatenated_path);
+			printf("command[1] + 1: %s\n", (command[1] + 1));
+		}
+		dup2(info->infile_fd, STDIN);
 		dup2(fd[1], STDOUT);
-		printf("%s\n", command[1] + 1);
 		err = execve(info->concatenated_path, &command[1] + 1, info->envp);
 		if (err == -1)
 			printf("failed execution of %s\n", command[0]);
 	}
+	waitpid(pid1, NULL, 0);
+	printf("this is the main process! \n");
 	pid2 = fork();
 	if (pid2 == 0)
 	{
 		dup2(fd[0], STDIN);
+		close(fd[1]);
 		if (DEBUG)
-			printf("executing %s\n", "grep");
-		command_2 = ft_split("\"test\"", ' ');
-		printf("%s\n", command_2[0]);
-		err = execve("/usr/bin/grep", &command_2[0], info->envp);
+			printf("executing %s\n", "cat");
+		err = execve("/bin/cat", NULL, info->envp);
 		if (err == -1)
-			printf("failed execution of %s\n", "grep");
+			printf("failed execution of %s\n", "cat");
 	}
-	waitpid(pid1, NULL, 0);
 	waitpid(pid2, NULL, 0);
 	return (1);
 }
